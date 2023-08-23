8960c167046d1f42e9ba2b70b187da752f7ff3b1
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-08-23 21:41:29 +0300

    ADD: benefits

---
+++ src/components/Benefits/Benefits.tsx
@@ -0,0 +1,15 @@
+import { SelectedPage } from "@/shared/types";
+
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+}
+
+
+export const Benefits = ({ setSelectedPage }: Props) => {
+    return (
+        <div>Benefits</div>
+    );
+}
+
+export default Benefits;
