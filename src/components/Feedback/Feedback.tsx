d8e70a79d5fc8d93018888339ba9a095d2f13e59
YuliiaMay <yuliiamaystrenko@gmail.com>
2023-09-01 14:10:14 +0300

    ADD: form

--- src/components/Feedback/Feedback.tsx
+++ src/components/Feedback/Feedback.tsx
@@ -1,7 +1,8 @@
 import Title from "@/shared/Title";
 import { SelectedPage } from "@/shared/types";
 import { motion } from "framer-motion";
-import ContactUsImg from "@/assets/images/ContactUs.jpg"
+import ContactUsImg from "@/assets/images/ContactUsPageImg.webp"
+import Form from "./Form";
 
 
 type Props = {
@@ -9,31 +10,53 @@ type Props = {
 }
 
 
-export const Feedback = ({setSelectedPage}: Props) => {
+export const Feedback = ({ setSelectedPage }: Props) => {
+
+
     return (
-        <section id="contactus" className="mx-auto min-h-full w-5/6 py-8">
+        <section id="contactus" className="mx-auto min-h-full">
             <motion.div
+                className="mx-auto w-5/6"
                 onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
             >
-                <motion.div
-                    className="md:w-3/5"
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5}}
-                    transition={{ duration: 0.5 }}
-                    variants={{
-                        hidden: { opacity: 0, x: -50 },
-                        visible: { opacity: 1, x: 0 },
-                    }}                    
-                >
-                    <Title>join now</Title>
-                    <p className="my-8">Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl sapien vel rhoncus. Placerat at in enim pellentesque. Nulla adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.</p>
-                </motion.div>
+                <Title>join now</Title>
 
+                <div className="md:flex gap-12">
+                    <motion.div
+                        className="md:w-1/2"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5}}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 },
+                        }}                    
+                    >
+                        
+                        <p className="my-8 text-lg">
+                            Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl sapien vel rhoncus. Placerat at in enim pellentesque. Nulla adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
+                        </p>
 
-                <div>
-                    <img src={ContactUsImg} alt="" />
+                        <Form/>
+                    </motion.div>
+
+                    <motion.div
+                        className="md:w-1/2"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5}}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: +400 },
+                            visible: { opacity: 1, x: 0 },
+                        }}                                        
+                    >
+                        <img src={ContactUsImg} alt="contact-us-img" />
+                    </motion.div>                    
+
                 </div>
+
             </motion.div>
         </section>
 
